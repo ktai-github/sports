486aaecbf68adb39ea2bea3207363be330c6098d
KevinT <kevin.tai.mail@gmail.com>
2017-10-27 12:52:01 -0700

    Initial Commit

---
+++ Sports/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sports
+//
+//  Created by KevinT on 2017-10-27.
+//  Copyright © 2017 kevint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
